2e76fa4ff5581e191bd9391c73d8a1a242a1049b
sumit10869 <142525697+sumit10869@users.noreply.github.com>
2024-06-26 07:10:33 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -19,15 +19,15 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 uvloop.install()
 MDB = "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn"
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None)
-MONGODB = config("MONGODB", default=MDB)
-OWN = 7065117445 # edit this
-GROUP = -1001878947221 # edit this
+API_ID = "21513517" #config("API_ID", default=None, cast=int)
+API_HASH = "838d3451485b95722878921877f12066" #config("API_HASH", default=None)
+BOT_TOKEN = "6839630607:AAERKjFuMly87MJH1-dxzY-WJCMlCv34iTw" #config("BOT_TOKEN", default=None)
+SESSION = "BQFxJpUADRb6nGpf6RWbfp2dsl6jQHaKVjYTP5-BZwNh707mM5BGBad8SXW5_mHLt20OISKWW-0h6kPi6pB9uNVsOZP_dot0TI6WygZsUOuDnuFFavNaptGOkSjQLKd3x4cCU5YoljEChwILuIjMOnw8obVHJA1Q8ZvUH1TgyS8uwl-aUQbmjsNNuuPFVD1BECfXw0bs4w14a6yhNLNGmFBII1E_9N40JyxKr8bQU4O_wOx-_amaoKtmLNQqLJBOcAXG9MPn9DqiJuvHkFyebCsROM2hoT2jtPReruXcqwwNhoccUtKfmrU9wS7DuL3UNK-MvVIc2yVCnb3ThXcEh1BUw4B00AAAAAGhhwirAA" #config("SESSION", default=None)
+FORCESUB = "official_satyam01" #config("FORCESUB", default=None)
+AUTH = "7004948651" #config("AUTH", default=None)
+MONGODB = "mongodb+srv://satyamyt10869:Sumit10869@cluster0.0ojjo0p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #config("MONGODB", default=MDB)
+OWN = 7004948651 # edit this
+GROUP = -1002238804331 # edit this
 OWNER_ID = int(config("OWNER_ID", default=OWN))
 LOG_GROUP = int(config("LOG_GROUP", default=GROUP))
 
